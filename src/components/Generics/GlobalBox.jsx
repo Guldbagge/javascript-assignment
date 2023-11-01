6dd30b18dabfe580310095eb3d923e42a3a8ff90
Edvin <edvin.thornholm@gmail.com>
2023-11-01 10:41:54 +0100

    Project and Case Studies  done

--- src/components/Generics/GlobalBox.jsx
+++ src/components/Generics/GlobalBox.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import newspaper from '../../assets/image/newspaper.png'
 
 const GlobalBox = ({img, title, url}) => {
   return (
     <Link className="project" to={url}>
-        <img src={img} alt="Image of newspaper a man reading"/>
+        <img src={img} alt={`Image of ${title}`} />
         <h3>{title}</h3>
         <div className="readmore">Read More <i className="fa-solid fa-arrow-up-right"></i></div> 
     </Link>
--- src/components/ProjectAndCase.jsx
+++ src/components/ProjectAndCase.jsx
@@ -3,15 +3,15 @@ import newspaper from '../assets/image/newspaper.png'
 import glasses from '../assets/image/glasses.png'
 import note from '../assets/image/note.png'
 import computer from '../assets/image/computer.png'
-import { Link } from 'react-router-dom'
 import GlobalBox from '../components/Generics/GlobalBox'
+import Button from './Generics/Button'
 
 const ProjectAndCase = () => {
     const services = [
-        {img: "newspaper", title: "Business Advice", url:"business"},
-        {title: "Startup Business", url:"startup"},
-        {title: "Financial Advice", url:"financial"},
-        {title: "Risk Management", url:"riskmanegment"},
+        { img: newspaper, title: "Business Advice", url: "business" },
+        { img: glasses, title: "Startup Business", url: "startup" },
+        { img: note, title: "Financial Advice", url: "financial" },
+        { img: computer, title: "Risk Management", url: "riskmanagement" },
     ]
   return (
     <section className="project-and-case">
@@ -25,37 +25,13 @@ const ProjectAndCase = () => {
 
         {   services.map ((service, index) => (
                 <GlobalBox key={index} img={service.img} title={service.title} url={service.url}/>
-                        
+          
             ))}
 
-
-            {/* <a className="project" href="#">
-                <img src={newspaper} alt="Image of newspaper a man reading"/>
-                <h3>Grow your business</h3>
-                <div className="readmore">Read More <i className="fa-solid fa-arrow-up-right"></i></div> 
-            </a>
-
-            <a className="project" href="#">
-                <img src={glasses} alt="Image of a pare glasses"/>
-                <h3>Why your client needs a responsive website</h3>
-                <div className="readmore">Read More <i className="fa-solid fa-arrow-up-right"></i></div> 
-            </a>
-
-            <a className="project" href="#">
-                <img src={note} alt="Image of notes"/>
-                <h3>Educate your employees to get better results</h3>
-                <div className="readmore">Read More <i className="fa-solid fa-arrow-up-right"></i></div> 
-            </a>
-
-            <a className="project" href="#">
-                <img src={computer}alt="Image of a computer"/>
-                <h3>Business Insights is a important piece of your business    </h3>
-                <div className="readmore">Read More <i className="fa-solid fa-arrow-up-right"></i></div> 
-            </a> */}
         </div>
 
         <div className="center-content">
-            <a className="btn-black" href="projects.html">All Recent Projects<i className="fa-solid fa-arrow-up-right"></i></a>
+            <Button type="dark" titel="All Recent Projects" url="/projects"/>
         </div>
     </div>
 </section> 
